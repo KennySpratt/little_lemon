7f16e63ecf3b7240df7b81faed650ab97ec1b1e0
Kenny Spratt <kspratt86@gmail.com>
2023-11-17 17:36:35 -0700

    CSS grid added

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,15 @@
+import logo from './assets/Logo.jpg';
+import { FaFacebookSquare } from "react-icons/fa";
+import { TiSocialTwitter } from "react-icons/ti";
+import { FaTiktok } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+
 function Footer() {
     return (
       <footer>
+        <div>
+            <img src={logo} alt="Little Lemon Logo" />
+        </div>
         <div className="footer-navigation">
           <ul>
             <li><a href="/home">Home</a></li>
@@ -11,21 +20,30 @@ function Footer() {
             <li><a href="/login">Login</a></li>
           </ul>
         </div>
-        
+
         <div className="footer-contact">
           <p>Address: 123 Lemon Street, Chicago IL </p>
           <p>Phone: (123) 456-7890</p>
           <p>Email: contact@littlelemon.com</p>
         </div>
-  
+
         <div className="footer-social">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
-          <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">TikTok</a>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <FaFacebookSquare />
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <FaInstagramSquare />
+          </a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+           <TiSocialTwitter />
+          </a>
+
+          <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
+          <FaTiktok />
+          </a>
         </div>
       </footer>
     );
   }
-  
+
 export default Footer;
-  
